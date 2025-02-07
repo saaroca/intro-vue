7aa27d25310ac869b616493e107f8ac9db3ec440
saaroca <saaroca@gmail.com>
2025-02-07 11:03:22 +0100

    add table feat add movies in table

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <TituloVue :title="texto" color="blue" />
+    <input
+      type="text"
+      placeholder="Escribe un titulo"
+      v-model="texto"
+      class="form-control"
+    />
+    <hr />
+    <div class="row mt-3">
+      <div class="col-lg-4">
+        <input
+          type="text"
+          placeholder="Titulo Pelicula"
+          v-model="titulo"
+          class="form-control"
+        />
+      </div>
+      <div class="col-lg-4">
+        <input
+          type="text"
+          placeholder="Año Pelicula"
+          v-model="año"
+          class="form-control"
+        />
+      </div>
+      <div class="col-lg-4">
+        <button class="btn btn-success btn-block" @click="agregar">
+          Agregar
+        </button>
+      </div>
+    </div>
+    <table class="table mt-3">
+      <thead>
+        <tr>
+          <th>Titulo</th>
+          <th>Año</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="pelicula in peliculas" :key="pelicula.titulo">
+          <td>{{ pelicula.titulo }}</td>
+          <td>{{ pelicula.año }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="alerta('Alerta')" class="btn btn-success">Click me</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TituloVue from "./components/Titulo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TituloVue,
+  },
+  data() {
+    return {
+      texto: "Mi texto",
+      titulo: "",
+      año: "",
+      peliculas: [
+        { titulo: "Terminator", año: "1995" },
+        { titulo: "Jurasic Park", año: "1996" },
+        { titulo: "Pokemon", año: "1997" },
+        { titulo: "Gladiator 2", año: "2025" },
+        { titulo: "Torrente", año: "2021" },
+      ],
+    };
+  },
+  methods: {
+    alerta(msg) {
+      alert(msg);
+    },
+    agregar() {
+      this.peliculas.push({
+        titulo: this.titulo,
+        año: this.año,
+      });
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Titulo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 v-bind:class="color">
+      {{ title }} <a :href="pagina">{{ texto }}</a>
+    </h1>
+    <Subtitulo />
+  </div>
+</template>
+
+<script>
+import Subtitulo from "./Subtitulo.vue";
+
+export default {
+  name: "TituloVue",
+  components: {
+    Subtitulo,
+  },
+  props: ["title", "color"],
+  data() {
+    return {
+      pagina: "https://vuejs.org/",
+      texto: "Vue",
+      target: "_blank",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+  font-weight: bolder;
+}
+
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+.brown {
+  color: brown;
+}
+</style>
